58d05dc3093974138140122e1ef27b961919973c
yueleng <wangyueleng@gmail.com>
2019-06-09 14:02:02 -0400

    Update app.js

--- cms/app.js
+++ cms/app.js
@@ -5,6 +5,17 @@ const exphbs = require('express-handlebars')
 
 console.log('__dirname:', __dirname)
 
+// all codes in static files have root directory: __dirname/public
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Set View Engine
+// views/layouts/home.handlebars
+// if exphbs() then app will search for views/layouts/main.handlebars
+app.engine('handlebars', exphbs({defaultLayout: 'home'})) 
+app.set('view engine', 'handlebars')
+
+// Load and use routes
+const home = require('./routes/home/index')
 const admin = require('./routes/admin/index')
 app.use('/', home)
 app.use('/admin', admin)
